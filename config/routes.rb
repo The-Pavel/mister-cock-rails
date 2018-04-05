70a78b33948401482bcf919e93da07d138a39935
Pavel Liser <liser.pavel@gmail.com>
2018-04-05 14:26:29 +0800

    created ingredients model, controller, database schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  # get 'ingredients/index'
+
+  # get 'ingredients/new'
+
+  # get 'ingredients/create'
+
+  # get 'ingredients/destroy'
+
+  resources :ingredients, only: [:new, :create, :destroy, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
