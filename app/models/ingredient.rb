7787daa21917f833dbc659eac63f7e2b6e4fed1a
Pavel Liser <liser.pavel@gmail.com>
2018-04-06 12:20:23 +0800

    added validation for one dose per ingredient and one ingredient per cocktail

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,6 +4,6 @@ class Ingredient < ApplicationRecord
   has_many :cocktails, through: :doses
 
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :cocktail, message: "Ingredient already exists in this cocktail"}
 
 end
